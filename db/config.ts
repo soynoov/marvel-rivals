d9716e50b426a8987c4c02172b84cd79041634ee
Soy_NooV <heroyi3000@gmail.com>
2025-01-03 16:01:52 +0100

    v_0.0.2:030125
    Update de las Tablas de la Base de Datos

--- db/config.ts
+++ db/config.ts
@@ -3,7 +3,20 @@ import { defineDb, defineTable, column } from 'astro:db';
 // https://astro.build/db/config
 
 const Heroe = defineTable({
-  columns: {},
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text(),
+    rol: column.number({ references: () => Rol.columns.id }), //FK
+    health: column.number({ unique: true }),
+    speed: column.number({ unique: true }),
+    damage: column.number({ unique: true }),
+  },
+});
+
+const Rol = defineTable({
+  columns: {
+    id: column.number(),
+  },
 });
 
 // export
